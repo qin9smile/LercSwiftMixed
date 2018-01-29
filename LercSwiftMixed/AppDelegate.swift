cb72f9cf34435b3fd28626f489e13839092fba0c
charlotte <charlotte@gagogroup.com>
2018-01-29 10:14:24 +0800

    init macos xcode project

---
+++ LercSwiftMixed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LercSwiftMixed
+//
+//  Created by charlotte on 2018/1/29.
+//  Copyright © 2018年 gago. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
