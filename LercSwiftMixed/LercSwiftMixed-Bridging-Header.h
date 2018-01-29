dc94d0ce95e50e6744f23bbc7982bdcd97c55017
charlotte <charlotte@gagogroup.com>
2018-01-29 14:39:41 +0800

    add bridging header

---
+++ LercSwiftMixed/LercSwiftMixed-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  LercSwiftMixed-Bridging-Header.h
+//  LercSwiftMixed
+//
+//  Created by charlotte on 2018/1/29.
+//  Copyright © 2018年 gago. All rights reserved.
+//
+
+#ifndef LercSwiftMixed_Bridging_Header_h
+#define LercSwiftMixed_Bridging_Header_h
+
+
+#endif /* LercSwiftMixed_Bridging_Header_h */
