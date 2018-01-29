c57cdd6bacdb41cf5d427e08d71151549b7fe2f6
charlotte <charlotte@gagogroup.com>
2018-01-29 14:42:53 +0800

    lerc wrapper

---
+++ LercWrapper/LercWrapper.h
@@ -0,0 +1,18 @@
+//
+//  LercWrapper.h
+//  LercSwiftMixed
+//
+//  Created by charlotte on 2018/1/29.
+//  Copyright © 2018年 gago. All rights reserved.
+//
+
+#ifndef LercWrapper_h
+#define LercWrapper_h
+#import <Foundation/Foundation.h>
+
+#endif /* LercWrapper_h */
+
+@interface LercWrapper : NSObject {
+
+}
+@end
---
+++ LercWrapper/LercWrapper.m
@@ -0,0 +1,17 @@
+//
+//  LercWrapper.m
+//  LercSwiftMixed
+//
+//  Created by charlotte on 2018/1/29.
+//  Copyright © 2018年 gago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import "LercWrapper.h"
+
+@implementation LercWrapper : NSObject {
+
+}
+
+@end
